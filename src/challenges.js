9bcd9160e7c0c809f383b53e19fe14ceef187c35
wladyrjusta <wladyr.justa@gmail.com>
2022-10-28 11:22:51 -0300

    function concatName tested && done.

--- src/challenges.js
+++ src/challenges.js
@@ -25,10 +25,15 @@ splitSentence('go Trybe');
 splitSentence('Vamo que Vamo');
 splitSentence('foguete');
 // Desafio 4
-function concatName() {
-  // seu código aqui
+const arrayString1 = ['Lucas', 'Cassiano', 'Ferraz', 'Paolillo'];
+const arrayString2 = ['foguete', 'não', 'tem', 'ré'];
+const arrayString3 = ['captain', 'my', 'captain'];
+function concatName(arrayStrings) {
+  return `${arrayStrings[arrayStrings.length - 1]} ${arrayStrings[0]}`;
 }
-
+concatName(arrayString1);
+concatName(arrayString2);
+concatName(arrayString3);
 // Desafio 5
 function footballPoints() {
   // seu código aqui
