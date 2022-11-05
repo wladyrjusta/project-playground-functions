a2d1545b8d77f3b760a9945b86d619032c5cfbe6
wladyrjusta <wladyr.justa@gmail.com>
2022-11-05 16:23:10 -0300

    #

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,45 +1,52 @@
 // Desafio 11
-const arrayPhoneId1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 6, 3];
-const arrayPhoneId2 = [1, 2, 3, 4, 5, 6, 7, 8, 6, 5, 1];
-const arrayPhoneId3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 2, 1];
-const arrayPhoneId4 = [1, 2, 3, 4, 5, 6, 7, 5, -1, 6, 6];
-const arrayPhoneId5 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2];
-function generatePhoneNumber(phoneId) {
-  var repeticao = 0;
-  for (let i1 in phoneId) {
-    let index1 = i1;
-    for (let i2 in phoneId) {
-      if (index1 === i2) {
-        repeticao = repeticao + 1;
+const arrayPhoneId1 = [0, 1, 6];
+const arrayPhoneId2 = [1, 2, 3, 4, -5, 6, 7, 8, 9, 0, 1];
+const arrayPhoneId3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1];
+const arrayPhoneId4 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 1];
+const arrayPhoneId5 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1];
+
+  function generatePhoneNumber(phoneId){
+      const countRepetition = numeros => {
+      let countRepeated = 0;
+      let countNumber = 0;
+      for (let index in numeros) {
+        let verifyNumber = numeros[index];
+        for (let index2 in numeros) {
+          if (verifyNumber === numeros[index2]) {
+            countNumber += 1;
+          }
+        }
+        if (countNumber > countRepeated) {
+          countRepeated = countNumber;
+        }
+        countNumber = 0;
+        }
+        return countRepeated;  
+      };
+
+    for (let index in phoneId) {
+      let phoneIdIndex = phoneId[index]; 
+      if (phoneId.length != 11) {
+        return `Array com tamanho incorreto.`;
+      } else if (phoneIdIndex < 0 || phoneIdIndex > 9 || countRepetition(phoneId) >= 3) {
+        return `não é possível gerar um número de telefone com esses valores`;
       }
     }
-  }
-  if (phoneId.length !== 11) {
-      return `Array com tamanho incorreto.`;
-  } else if (repeticao >= 3) {
-      return `não é possível gerar um número de telefone com esses valores`;
-  };
-  for (var phoneIdIndex = 0; phoneIdIndex < phoneId.length; phoneIdIndex += 1) {
-    if (phoneId[phoneIdIndex] < 0) {
-      return `não é possível gerar um número de telefone com esses valores`;
-    } else if (phoneId[phoneIdIndex] > 9) {
-      return `não é possível gerar um número de telefone com esses valores`;
-    } else {
-      return `(12) 34567-8901`;
-    }
-  }
-}
-    
-
- 
+    let gerarNum = phoneId;
+  gerarNum.splice(0, 0, '(');
+  gerarNum.splice(3, 0, ')');
+  gerarNum.splice(4, 0, ' ');
+  gerarNum.splice(10, 0, '-');
+  return gerarNum.join('');
+  } 
 console.log(generatePhoneNumber(arrayPhoneId1));
 console.log(generatePhoneNumber(arrayPhoneId2));
 console.log(generatePhoneNumber(arrayPhoneId3));
 console.log(generatePhoneNumber(arrayPhoneId4));
 console.log(generatePhoneNumber(arrayPhoneId5));
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck( ) {
+
 }
 
 // Desafio 13
