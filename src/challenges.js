3bc29f786335ad0daf10813c98f4dad28efe4712
wladyrjusta <wladyr.justa@gmail.com>
2022-11-19 13:32:14 -0300

    #

--- src/challenges.js
+++ src/challenges.js
@@ -6,35 +6,35 @@ function compareTrue(animal1, animal2) {
   if (animal1 && animal2 === true) {
     return true;
   } return false;
-}
+};
 compareTrue(macaco, elefante);
 compareTrue(macaco, macaco);
 compareTrue(girafa, elefante);
 // Desafio 2
 function calcArea(base, heigth) {
   return (base * heigth) / 2;
-}
+};
 calcArea(10, 50);
 calcArea(5, 2);
 calcArea(51, 1);
 // Desafio 3
 function splitSentence(frase) {
   return frase.split(' ');
-}
+};
 splitSentence('go Trybe');
 splitSentence('Vamo que Vamo');
 splitSentence('foguete');
 // Desafio 4
 function concatName(arrayStrings) {
   return `${arrayStrings[arrayStrings.length - 1]}, ${arrayStrings[0]}`;
-}
+};
 concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']);
 concatName(['foguete', 'não', 'tem', 'ré']);
 concatName(['captain', 'my', 'captain']);
 // Desafio 5
 function footballPoints(wins, ties) {
   return (wins * 3) + ties;
-}
+};
 footballPoints(14, 8);
 footballPoints(1, 2);
 footballPoints(0, 0);
@@ -51,10 +51,10 @@ function highestCount(arrayNums) {
     }
   }
   return indexX;
-}
-console.log(highestCount(arrayNum1));
-console.log(highestCount(arrayNum2));
-console.log(highestCount(arrayNum3));
+};
+highestCount(arrayNum1);
+highestCount(arrayNum2);
+highestCount(arrayNum3);
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
   if (Math.abs(mouse - cat1) > Math.abs(mouse - cat2)) {
@@ -63,18 +63,16 @@ function catAndMouse(mouse, cat1, cat2) {
     return 'os gatos trombam e o rato foge';   
   } else if (Math.abs(mouse - cat1) < Math.abs(mouse - cat2)) {
     return'cat1'; 
- }
-}
-console.log(catAndMouse(0, 3, 2));
-console.log(catAndMouse(0, 6, 12));
-console.log(catAndMouse(0, 3, 3));
+  }
+};
+catAndMouse(0, 3, 2);catAndMouse(0, 6, 12);catAndMouse(0, 3, 3);
 // Desafio 8
 const array1 = [2, 15, 7, 9, 45];
 const array2 = [7, 9];
 const array3 = [9, 25];
 function fizzBuzz(arrayNumString) {
- var strings = []; 
- for (var index of arrayNumString) {
+  var strings = []; 
+  for (var index of arrayNumString) {
   if (index % 3 === 0 && index % 5 === 0) {
     strings.push('fizzBuzz');
   } else if (index % 5 === 0) {
@@ -84,12 +82,12 @@ function fizzBuzz(arrayNumString) {
   } else {
     strings.push('bug!')
   } 
- }
+ };
  return strings;
-}
-console.log(fizzBuzz(array1));
-console.log(fizzBuzz(array2));
-console.log(fizzBuzz(array3));
+};
+fizzBuzz(array1);
+fizzBuzz(array2);
+fizzBuzz(array3);
 // Desafio 9
 function encode(arrayEncode) {
 arrayEncode = arrayEncode.replace(/a/g, 1);
@@ -98,12 +96,12 @@ arrayEncode = arrayEncode.replace(/i/g, 3);
 arrayEncode = arrayEncode.replace(/o/g, 4);
 arrayEncode = arrayEncode.replace(/u/g, 5);
 return arrayEncode;
-}
-console.log(encode('hello'));
-console.log(encode('How are you today?'));
-console.log(encode('This is an enconding test.'));
-console.log(encode('go Trybe'));
-////########
+};
+encode('hello');
+encode('How are you today?');
+encode('This is an enconding test.');
+encode('go Trybe');
+////#######
 function decode(arrayDecode) {
 arrayDecode = arrayDecode.replace(/1/g, 'a');
 arrayDecode = arrayDecode.replace(/2/g, 'e');
@@ -111,13 +109,27 @@ arrayDecode = arrayDecode.replace(/3/g, 'i');
 arrayDecode = arrayDecode.replace(/4/g, 'o');
 arrayDecode = arrayDecode.replace(/5/g, 'u');
 return arrayDecode;  
-}
-console.log(decode('h3 th2r2!'))
+};
+decode('h3 th2r2!');
 // Desafio 10
-function techList() {
-  // seu código aqui
-}
-
+const arrayTechs = ['React', 'Jest', 'HTML', 'CSS', 'JavaScript'];
+const name = 'Lucas';
+function techList(arrayTechnologies, name) {
+  if (arrayTechnologies.length === 0) {
+    return 'Vazio!'
+  };
+  let arrayTech = arrayTechnologies.sort();
+  let arrayOfObject = [];
+  for (let index = 0; index < arrayTechnologies.length; index += 1) {
+    let objectTechs = {
+      name: name,
+      tech: arrayTech[index]
+    };
+    arrayOfObject.push(objectTechs); 
+  };
+  return arrayOfObject
+};
+techList(arrayTechs, name);
 module.exports = {
   calcArea,
   catAndMouse,
--- src/challenges2.js
+++ src/challenges2.js
@@ -4,34 +4,33 @@ const arrayPhoneId2 = [1, 2, 3, 4, -5, 6, 7, 8, 9, 0, 1];
 const arrayPhoneId3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1];
 const arrayPhoneId4 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 1];
 const arrayPhoneId5 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1];
-
-  function generatePhoneNumber(phoneId){
-      const countRepetition = numeros => {
-      let countRepeated = 0;
-      let countNumber = 0;
-      for (let index in numeros) {
-        let verifyNumber = numeros[index];
-        for (let index2 in numeros) {
-          if (verifyNumber === numeros[index2]) {
-            countNumber += 1;
+function generatePhoneNumber(phoneId){
+const countRepetition = numeros => {
+let countRepeated = 0;
+let countNumber = 0;
+  for (let index in numeros) {
+    let verifyNumber = numeros[index];
+      for (let index2 in numeros) {
+        if (verifyNumber === numeros[index2]) {
+          countNumber += 1;
           }
-        }
-        if (countNumber > countRepeated) {
-          countRepeated = countNumber;
-        }
-        countNumber = 0;
-        }
-        return countRepeated;  
+        };
+    if (countNumber > countRepeated) {
+      countRepeated = countNumber;
       };
-
-    for (let index in phoneId) {
-      let phoneIdIndex = phoneId[index]; 
-      if (phoneId.length != 11) {
-        return `Array com tamanho incorreto.`;
-      } else if (phoneIdIndex < 0 || phoneIdIndex > 9 || countRepetition(phoneId) >= 3) {
-        return `não é possível gerar um número de telefone com esses valores`;
-      }
+      countNumber = 0;
     }
+    return countRepeated;  
+  };
+
+  for (let index in phoneId) {
+    let phoneIdIndex = phoneId[index]; 
+    if (phoneId.length != 11) {
+      return `Array com tamanho incorreto.`;
+    } else if (phoneIdIndex < 0 || phoneIdIndex > 9 ||    countRepetition(phoneId) >= 3) {
+      return `não é possível gerar um número de telefone com esses valores`;
+      }
+    };
     let gerarNum = phoneId;
   gerarNum.splice(0, 0, '(');
   gerarNum.splice(3, 0, ')');
